9335668bd2344693874dd4928ebb8283d864ffab
Enrico Bruno Del Zotto <delzotto.enrico@gmail.com>
2022-01-06 18:03:57 +0100

    Adding repository mapper test

---
+++ data/network/src/test/java/com/ebdz/network/mapper/RepositoryMapperTest.kt
@@ -0,0 +1,54 @@
+package com.ebdz.network.mapper
+
+import android.text.Spannable
+import com.ebdz.network.GetStarredRepositoriesFromOrganizationNameQuery
+import com.ebdz.repository.model.Repository
+import junit.framework.TestCase.assertEquals
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class RepositoryMapperTest {
+    private lateinit var underTest: RepositoryMapper
+
+    @Mock
+    private lateinit var spanString: Spannable
+
+    @Before
+    fun setup() {
+        underTest = RepositoryMapper()
+    }
+
+    @Test
+    fun shouldMapRepositoryCorrectly() {
+        val stub = GetStarredRepositoriesFromOrganizationNameQuery.OnRepository(
+            id = "id",
+            name = "name",
+            openGraphImageUrl = "http://www.anImage.it".toHttpUrlOrNull()!!,
+            stargazerCount = 0,
+            url = "http://www.google.it".toHttpUrlOrNull()!!,
+            shortDescriptionHTML = spanString
+        )
+
+
+        given(spanString.toString()).willReturn("test")
+
+        val result = Repository(
+            id = "id",
+            name = "name",
+            openGraphImageUrl = "http://www.animage.it/",
+            stargazerCount = 0,
+            url = "http://www.google.it/",
+            shortDescriptionHTML = "test"
+        )
+
+        val toCheck = underTest.toRepository(stub)
+
+        assertEquals(toCheck, result)
+    }
+}
