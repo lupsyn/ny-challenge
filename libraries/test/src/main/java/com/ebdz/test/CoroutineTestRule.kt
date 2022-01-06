b0d49fa4bbd6c8568dbc70b7a5c8033ec99a8b60
Enrico Bruno Del Zotto <delzotto.enrico@gmail.com>
2022-01-06 14:39:43 +0100

    Adding coroutine test rule

--- libraries/test/src/main/java/com/ebdz/test/CoroutineTestRule.kt
+++ libraries/test/src/main/java/com/ebdz/test/CoroutineTestRule.kt
@@ -1,26 +1,26 @@
 package com.ebdz.test
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class TestCoroutineRule(
-    private val testCoroutineDispatcher: CoroutineDispatcher = UnconfinedTestDispatcher()
+class CoroutineTestRule(
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
 ) : TestWatcher() {
 
     override fun starting(description: Description?) {
-        Dispatchers.setMain(testCoroutineDispatcher)
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description?) {
+        super.finished(description)
         Dispatchers.resetMain()
-        testCoroutineDispatcher.cancel()
+        testDispatcher.cleanupTestCoroutines()
     }
 }
